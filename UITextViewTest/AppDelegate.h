3f9949db8ce357a6103913be914afbc62b94e7b6
chuanying.lei <chuanying.lei@zhongtoutiandi.com>
2016-06-22 15:41:30 +0800

    Initial Commit

---
+++ UITextViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITextViewTest
+//
+//  Created by 雷传营 on 16/6/22.
+//  Copyright © 2016年 Flying-Einstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITextViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextViewTest
+//
+//  Created by 雷传营 on 16/6/22.
+//  Copyright © 2016年 Flying-Einstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
